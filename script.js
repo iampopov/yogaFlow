f35861863c5b73f665f7efd8b39a052229748bce
IamPopov <ivantsar@gmail.com>
2019-12-31 17:26:51 -0700

    added page structure

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
-$('<h1>').appendTo('#body').text('helllo')
-$('<img>').attr('src', yogaPosesArr[0].img_url).appendTo('#body')
+$('<h1>').appendTo('#main').text('helllo')
+$('<img>').attr('src', yogaPosesArr[0].img_url).appendTo('#main')
