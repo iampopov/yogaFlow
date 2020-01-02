10b86f6dc5386ea8467d3838324295f644931671
IamPopov <ivantsar@gmail.com>
2020-01-01 21:44:51 -0700

    created a for loop showing all poses for Create Flow constructor and a basic algorithm to cycle through an array of selcted poses on timer

--- script.js
+++ script.js
@@ -1,2 +1,61 @@
+
+
 $('<h1>').appendTo('#main').text('helllo')
-$('<img>').attr('src', yogaPosesArr[0].img_url).appendTo('#main')
+function showPoses () {
+    for (var i = 0; i < yogaPosesArr.length; i++) {
+        var cardDiv = $('<div>').attr({
+            class: "card"
+        });
+
+        var cardImgDiv = $('<div>').attr({
+            class: "image"
+        });
+        cardImgDiv.appendTo(cardDiv);
+        
+        var cardImage = $('<img>').attr({
+            src: yogaPosesArr[i].img_url
+        });
+        cardImage.appendTo(cardImgDiv);
+
+        var cardCont = $('<div>').attr({
+            class: "content"
+        });
+
+        cardCont.appendTo(cardDiv);
+
+        var cardHeader = $('<div>').attr({
+            class: "header"
+        }).text(yogaPosesArr[i].id)
+       
+        cardHeader.appendTo(cardCont);
+
+        var cardDesc = $('<div>').attr({
+            class: "description"
+        }).text(yogaPosesArr[i].english_name+" or "+yogaPosesArr[i].sanskrit_name)
+
+        cardDesc.appendTo(cardCont);
+
+        cardDiv.appendTo("#poses");
+    };
+};
+
+$('#main').ready(function(){
+    var exampleFlow = [7, 8, 15, 14, 26, 30, 31],
+          counter = 0,
+          timer = setInterval(function(){
+                showPose(exampleFlow[counter]);
+                counter++
+                if (counter === exampleFlow.length) {
+                      clearInterval(timer);
+                }
+          },2000);
+
+    function showPose (poses) {
+    // console.log(poses);
+    $('<img>').attr('src', yogaPosesArr[poses-1].img_url).prependTo('#main')
+
+    }
+});
+
+$('#topMenu').on("click", "#createFlow", showPoses);
+
